0d099ae440676b745f8e6772af4d6b9578e5a607
s7alalawi <202109507@stu.uob.edu.bh>
2024-05-31 18:40:35 +0300

    Almost ready

--- Client.py
+++ Client.py
@@ -51,6 +51,49 @@ Categories = ["business", "entertainment", "general", "health", "science" "sport
 Countries = ["au", "nz", "ca", "ae", "sa", "gb", "us", "eg", "ma"]
 Languages =["ar", "en"]
 
+
+
+
+
+
+def headlines(Client_socket):
+     HData = Client_socket.recv(200000).decode()
+     List_details = json.loads(HData)
+     print("-------------------- | The headlines:| -----------------\n")
+     for index, item in enumerate(List_details['Articles']):
+          print(f"{index+1}. {item['Title']} -{item['description']} ")
+     headlines_number = input("Enter the number of articles you want: ")
+     headlines_want = List_details['Articles'][headlines_number - 1]
+
+     print("Article NO.{headline_number} details:\n")
+     print(f"Source Name: {headlines_want['source']['name']}")
+     print(f"Author: {headlines_want['author']}")
+     print(f"Title: {headlines_want['title']}")
+     print(f"URL: {headlines_want['url']}")
+     print(f"Description: {headlines_want['description']}")
+     print(f"Published Date & Time: {headlines_want['publishedAt']}")
+
+
+def sources(Client_socket):
+     SData = Client_socket.recv(200000).decode()
+     List_details = json.loads(SData)
+     print("-------------------- | The sources:| -----------------\n")
+     for index, item in enumerate(List_details['sources']):
+          print(f"{index+1}. {item['name']}")
+     
+     sources_number = input("Enter the number of sources you want: ")
+     sources_want = List_details['sources'][sources_number - 1]
+     
+     print("Source NO.{sources_number} details:\n")
+     print(f"Name: {sources_want['name']}")
+     print(f"Country: {sources_want['country']}")
+     print(f"Description: {sources_want['description']}")
+     print(f"URL: {sources_want['url']}")
+     print(f"Category: {sources_want['category']}")
+     print(f"Language: {sources_want['language']}")
+
+
+
 while True:
  opt = display_main_menu
  opt_headlines = display_headlines_menu
@@ -62,7 +105,7 @@ while True:
             keyword = input("Enter keyword: ")
             opt = f"1. {opt_headlines}.{keyword}"
             Client_send = (opt, client_socket)
-           # headlines(client_socket)
+            headlines(client_socket)
        elif opt_headlines == 2:
       
             print("[business, entertainment, general, health, science, sports, technology]")
@@ -72,7 +115,7 @@ while True:
                  
             opt = f"1. {opt_headlines}.{category}"
             Client_send = (opt, client_socket)
-            # headlines(client_socket)
+            headlines(client_socket)
 
        elif opt_headlines == 3:
            print("[au, nz, ca, ae, sa, gb, us, eg, ma]")
@@ -82,12 +125,12 @@ while True:
             
            opt = f"1. {opt_headlines}.{country}"
            Client_send = (opt, client_socket)
-           # headlines(client_socket)
+           headlines(client_socket)
        
        elif opt_headlines == 4:
             opt = f"1. {opt_headlines}.null"
             Client_send(opt,client_socket)
-            # headlines(client_socket)
+            headlines(client_socket)
        
        elif opt_headlines == 5:
             print("123")
@@ -107,7 +150,7 @@ while True:
                  #return
             opt = f"2. {opt_sources}.{category}"
             Client_send = (opt, client_socket)
-            # sources(client_socket)
+            sources(client_socket)
             
              
        elif opt_sources == 2:
@@ -118,7 +161,7 @@ while True:
             
            opt = f"1. {opt_sources}.{country}"
            Client_send = (opt, client_socket)
-           # sources(client_socket)
+           sources(client_socket)
        
        elif opt_sources == 3:
             print("[ar, en]")
@@ -128,12 +171,12 @@ while True:
                  #return
             opt = f"2. {opt_sources}.{Language}"
             Client_send = (opt, client_socket)
-            # sources(client_socket)
+            sources(client_socket)
        
        elif opt_sources == 4:
             opt = f"{opt_sources}.null"
             Client_send = (opt, client_socket)
-            # sources(client_socket)
+            sources(client_socket)
        elif opt_sources == 5:
             print("123")
             #return
@@ -147,3 +190,16 @@ while True:
        break
  else:
        print("Invalid option. Please try again.")
+
+
+
+
+def Client_send(opt, Client_socket):
+     Client_socket.send(opt.encode())
+
+
+
+if __name__ == "__main__":
+     display_main_menu()
+
+
