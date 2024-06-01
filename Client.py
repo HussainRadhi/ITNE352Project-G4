24a503e7c4488a504de6a9fc895980263edf7ea2
s7alalawi <202109507@stu.uob.edu.bh>
2024-06-02 01:16:37 +0300

    update

--- Client.py
+++ Client.py
@@ -109,3 +109,110 @@ def print_selected_source(data):
             print(tabulate(table_data,headers=["Source Name", "Country", "Description", "URL", "Category", "Language"], tablefmt="grid"))
     except json.JSONDecodeError:
         print(data)  # Print the raw string data if decoding fails
+def serve_client():
+    try:
+        # Getting the username from the user
+        username = ""
+        while username == "":
+            username = str(input("Please write your name: "))
+        # Sending the username to the server
+        client.sendall(username.encode())
+        # Displaying the list of clients connected to the server
+        print("clients in the server: " + receive_full_data(client))
+
+        # Continuous loop to interact with the server
+        while True:
+            print("Select One from the options:")
+            print("1- Search Headlines.")
+            print("2- List of Sources")
+            print("3- Quit")
+            # Getting the option selected by the user
+            option_request = int(input())
+            # Handling different options selected by the user
+            if option_request == 1:
+                while True:
+                    print("Search Headlines Menu:")
+                    print("1- Search for Keywords.")
+                    print("2- Search by Category")
+                    print("3- Search by Country")
+                    print("4- List all new Headlines")
+                    print("5- Back to The Main Menu")
+                    # Getting the option selected by the user
+                    second_option_request = int(input())
+                    # Handling different options selected by the user
+                    if second_option_request == 1:
+                        keyword = ""
+                        client.sendall("1".encode())
+                        while keyword == "":
+                            keyword = str(input("Please input search keyword: "))
+                        client.sendall(keyword.encode())
+                        json_data = receive_full_data(client)
+                        try:
+                            data_dict = json.loads(json_data)
+                            articles = data_dict.get('articles', [])
+                            idx = print_table_articles(articles)
+                        except json.JSONDecodeError as e:
+                            print(f"Failed to decode JSON: {e}")
+                            print("Received data:", json_data)
+                        selected_article = ""
+                        while not selected_article.isdigit() or not (1 <= int(selected_article) <= idx):
+                            selected_article = str(input("Please Enter Article Number: "))
+                        client.sendall(selected_article.encode())
+                        print_selected_article_headlines(receive_full_data(client))
+                    elif second_option_request == 2:
+                        client.sendall("2".encode())
+                        valid_categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+                        category = ""
+                        while category.lower() not in valid_categories:
+                            category = input("Please input search category (business, entertainment, general, health, science, sports, technology): ").strip()
+                        client.sendall(category.encode())
+                        json_data = receive_full_data(client)
+                        try:
+                            data_dict = json.loads(json_data)
+                            articles = data_dict.get('articles', [])
+                            idx = print_table_articles(articles)
+                        except json.JSONDecodeError as e:
+                            print(f"Failed to decode JSON: {e}")
+                            print("Received data:", json_data)
+                        selected_article = ""
+                        while not selected_article.isdigit() or not (1 <= int(selected_article) <= idx):
+                            selected_article = str(input("Please Enter Article Number: "))
+                        client.sendall(selected_article.encode())
+                        print_selected_article_headlines(receive_full_data(client))
+                    elif second_option_request == 3:
+                        client.sendall("3".encode())
+                        valid_countries = ["au", "nz", "ca", "ae", "sa", "gb", "us", "eg", "ma"]
+                        country = ""
+                        while country.lower() not in valid_countries:
+                            country = input("Please input search country (au, nz, ca, ae, sa, gb, us, eg, ma): ").strip()
+                        client.sendall(country.encode())
+                        json_data = receive_full_data(client)
+                        try:
+                            data_dict = json.loads(json_data)
+                            articles = data_dict.get('articles', [])
+                            idx = print_table_articles(articles)
+                        except json.JSONDecodeError as e:
+                            print(f"Failed to decode JSON: {e}")
+                            print("Received data:", json_data)
+                        selected_article = ""
+                        while not selected_article.isdigit() or not (1 <= int(selected_article) <= idx):
+                            selected_article = str(input("Please Enter Article Number: "))
+                        client.sendall(selected_article.encode())
+                        print_selected_article_headlines(receive_full_data(client))
+                    elif second_option_request == 4:
+                        client.sendall("4".encode())
+                        json_data = receive_full_data(client)
+                        try:
+                            data_dict = json.loads(json_data)
+                            articles = data_dict.get('articles', [])
+                            idx = print_table_articles(articles)
+                        except json.JSONDecodeError as e:
+                            print(f"Failed to decode JSON: {e}")
+                            print("Received data:", json_data)
+                        selected_article = ""
+                        while not selected_article.isdigit() or not (1 <= int(selected_article) <= idx):
+                            selected_article = str(input("Please Enter Article Number: "))
+                        client.sendall(selected_article.encode())
+                        print_selected_article_headlines(receive_full_data(client))
+                    elif second_option_request == 5:
+                        break
