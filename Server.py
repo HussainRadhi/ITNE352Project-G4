284ad28469907134ab1d445cd98506974ccd3bbc
HussainRadhi <95987846+HussainRadhi@users.noreply.github.com>
2024-06-01 22:18:59 +0300

    Server.py

--- Server.py
+++ Server.py
@@ -1,8 +1,96 @@
 import socket
 import threading
 import json
-import os
+import requests
+import ssl
+# List to keep track of connected clients
+clients_list = []
 
-from newsapi import NewsApiClient
 
-API = NewsApiClient(APIK= '42fb59665f644bdfa9aaab9e4de20b14')
+def save_f_data(data, type, name, request_type):
+    # Placeholder function, you need to implement this
+    file_name = f"G4_{name}_{type}_{request_type}.json"
+    with open(file_name, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+    return file_name
+
+
+# Functions to process client requests
+
+def headlines_request(request_type, filter_criteria, name, article_number):
+    ApiKey = "92caab6f774548e2852b1aa6ceed17b7"
+    try:
+        if request_type in ["country", "q", "category","All"]:
+            if article_number == "":
+                if request_type != "All":
+                    request_url = f"https://newsapi.org/v2/top-headlines?{request_type.lower()}={filter_criteria}&apiKey={ApiKey}&pageSize=15&page=1"
+                else:
+                    request_url = f"https://newsapi.org/v2/top-headlines?language=en&apiKey={ApiKey}&pageSize=15&page=1"
+                try:
+                    response = requests.get(request_url)
+                    response.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    return f"Request failed: {e}"
+                try:
+                    data = response.json()
+                except json.JSONDecodeError:
+                    return "Failed to parse the response JSON."
+                if 'articles' in data:
+                    articles = data['articles']
+                    if articles:
+                        try:
+                            save_f_data(data,"headlines", name, request_type)
+                        except IOError as e:
+                            return f"Failed to save data to file: {e}"
+                        json_data = json.dumps(data, ensure_ascii=False)
+                        return json_data
+                    else:
+                        return "No articles found in the response."
+                else:
+                    return "No 'articles' key found in the response."
+            else:
+                try:
+                    with open(f"G4_{name}_headlines_{request_type}.json", 'r', encoding='utf-8') as file:
+                        data = json.load(file)
+                except FileNotFoundError:
+                    return ("File not found.")
+                except json.JSONDecodeError:
+                    return ("Failed to parse the saved JSON file.")
+                except IOError as e:
+                    return (f"Failed to read file: {e}")
+
+                articles_info = []
+                articles = data.get('articles', [])
+                # Check if there are articles in the data
+                article_number = int(article_number)-1
+                if articles:
+                    # Check if the specified index is within the range of available articles
+                    if 0 <= article_number < len(articles):
+                        article = articles[article_number]
+
+                        published_at = article.get('publishedAt', 'Unknown')
+                        if published_at != 'Unknown':
+                            date, time = published_at.split('T')
+                            time = time.split('Z')[0]
+                        else:
+                            date, time = 'Unknown', 'Unknown'
+                        info = {
+                            'source': article.get('source', {}).get('name', 'Unknown'),
+                            'author': article.get('author', 'Unknown'),
+                            'title': article.get('title', 'No title'),
+                            'url': article.get('url', 'Unknown'),
+                            'description': article.get('description', 'No description'),  # Get the source name
+                            'publish_date': date,
+                            'publish_time': time
+                        }
+                        articles_info.append(info)
+                    else:
+                        return(f"Article at index {article_number} not found.")
+                else:
+                    return("No articles found in the data.")
+                return json.dumps(articles_info, ensure_ascii=False)
+    except Exception as e:
+        return f"An unexpected error occurred 1: {e}"
+
+def sources_request(request_type, filter_criteria, name, source_number):
+    ApiKey = "92caab6f774548e2852b1aa6ceed17b7"
